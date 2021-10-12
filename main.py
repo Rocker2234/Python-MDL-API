8fc544b1a3765c80b7a73f5f27362d1abfdd8872
Thakshak S.K <thakshak22@gmail.com>
2021-10-12 15:47:06 +0530

    Scraping top 20 Results

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import bs4
 import requests
 
 
+movie = input("Name:")
+
 try:
-    url = f"https://mydramalist.com/search?q={'Move to Heaven'.replace(' ', '+')}"
+    url = f"https://mydramalist.com/search?q={movie.replace(' ', '+')}"
     a = requests.get(url)
 except requests.exceptions.ConnectionError:
     print("Problem connecting! Check your internet connection!")
@@ -11,4 +13,25 @@ except requests.exceptions.ConnectionError:
 
 # noinspection PyUnboundLocalVariable
 soup = bs4.BeautifulSoup(a.text, 'lxml')
-soup.find()
+results_page = soup.find('div', class_='col-lg-8 col-md-8').find_all('div', class_='box')
+print(len(results_page))
+
+results = {}
+
+for item in results_page:
+    # Titles
+    curr_title = item.find("h6").find('a').text
+    results[curr_title] = []
+    # Category
+    results[curr_title].append(item.find('span', class_='text-muted').text)
+    # Score
+    results[curr_title].append(item.find('span', class_='score').text)
+    # Desc
+    # print(item.find_all('p')[1].text)
+    # print(curr_title, ":", results[curr_title])
+
+    print('\n ----------- \n')
+
+for key in results.keys():
+    print(f"{key}: {results[key]}")
+    print('\n ----------- \n')
