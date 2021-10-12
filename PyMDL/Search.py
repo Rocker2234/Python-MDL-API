5ddd1fec49ad51051446197b055ed70bbce6161c
Thakshak S.K <thakshak22@gmail.com>
2021-10-12 21:30:25 +0530

    Added ability to filter

--- PyMDL/Search.py
+++ PyMDL/Search.py
@@ -2,34 +2,57 @@ import bs4
 import requests
 
 
-movie = input("Name:")
+class Search:
+    def __init__(self):
+        self.items = 0
+        self.names = []
 
-try:
-    url = f"https://mydramalist.com/search?q={movie.replace(' ', '+')}"
-    a = requests.get(url)
-except requests.exceptions.ConnectionError:
-    print("Problem connecting! Check your internet connection!")
-    exit(1)
+    def search(self, name: str, style: str = None, year: int = None, eps: int = None, max_results=20):
+        if max_results > 20:
+            print("Cannot have more than 20 Results!")
+            max_results = 20
+        url = f"https://mydramalist.com/search?q={name.replace(' ', '+')}"
+        base = requests.get(url)
+        # noinspection PyUnboundLocalVariable
+        soup = bs4.BeautifulSoup(base.text, 'lxml')
+        results_box = soup.find('div', class_='col-lg-8 col-md-8').find_all('div', class_='box')
+        filter_check = [True, True, True]  # This is to check for filters, [0] is style, [1] is year, [2] is eps
+        for item in results_box[:max_results]:
+            # Title
+            curr_title = item.find("h6").find('a').text
+            curr_cateory = item.find('span', class_='text-muted').text
+            print(curr_cateory)
 
-# noinspection PyUnboundLocalVariable
-soup = bs4.BeautifulSoup(a.text, 'lxml')
-results_page = soup.find('div', class_='col-lg-8 col-md-8').find_all('div', class_='box')
+            # Check for Episodes Filter
+            if eps:
+                if not ((curr_cateory.split(',')[-1]) == f" {eps} episodes"):
+                    filter_check[2] = False
 
-results = {}
+            # Check for Year Filter
+            if year:
+                if not curr_cateory.split(',')[0].split('-')[-1].strip() == str(year):
+                    filter_check[1] = False
 
-for item in results_page:
-    # Titles
-    curr_title = item.find("h6").find('a').text
-    results[curr_title] = []
-    # Category
-    results[curr_title].append(item.find('span', class_='text-muted').text)
-    # Score
-    results[curr_title].append(item.find('span', class_='score').text)
-    # Desc
-    # print(item.find_all('p')[1].text)
-    # print(curr_title, ":", results[curr_title])
+            # Check for Style Filter
+            if style:
+                if curr_cateory.find(style) == -1:
+                    filter_check[0] = False
 
-print("Found", len(results), "Results!")
-for key in results.keys():
-    print(f"{key}: {results[key]}")
-    print('\n ----------- \n')
+            # Add it to list if checks pass
+            if filter_check == [True, True, True]:
+                self.names.append(curr_title)
+            # Category
+            # results[curr_title].append(item.find('span', class_='text-muted').text)
+            # Score
+            # results[curr_title].append(item.find('span', class_='score').text)
+            # Description
+            # results[curr_title].append(item.find_all('p')[1].text)
+            self.items = len(self.names)
+
+
+if __name__ == "__main__":
+    res = Search()
+    # print(res.items)
+    res.search('Better Days', style='Filipino', year=2020, eps=6)
+    print("Items:", res.items)
+    print(res.names)
