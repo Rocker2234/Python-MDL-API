fa621b3feb0984410f19e4fb489fb221b13f6c1d
Thakshak S.K <thakshak22@gmail.com>
2021-10-12 21:41:21 +0530

    Minor Bug Fix

--- PyMDL/Search.py
+++ PyMDL/Search.py
@@ -16,16 +16,15 @@ class Search:
         # noinspection PyUnboundLocalVariable
         soup = bs4.BeautifulSoup(base.text, 'lxml')
         results_box = soup.find('div', class_='col-lg-8 col-md-8').find_all('div', class_='box')
-        filter_check = [True, True, True]  # This is to check for filters, [0] is style, [1] is year, [2] is eps
         for item in results_box[:max_results]:
             # Title
             curr_title = item.find("h6").find('a').text
             curr_cateory = item.find('span', class_='text-muted').text
-            print(curr_cateory)
+            filter_check = [True, True, True]  # This is to check for filters, [0] is style, [1] is year, [2] is eps
 
             # Check for Episodes Filter
             if eps:
-                if not ((curr_cateory.split(',')[-1]) == f" {eps} episodes"):
+                if not ((curr_cateory.split(',')[-1]).startswith(f" {eps} episode")):
                     filter_check[2] = False
 
             # Check for Year Filter
@@ -53,6 +52,6 @@ class Search:
 if __name__ == "__main__":
     res = Search()
     # print(res.items)
-    res.search('Better Days', style='Filipino', year=2020, eps=6)
+    res.search('Flower of evil', year=2020)
     print("Items:", res.items)
     print(res.names)
