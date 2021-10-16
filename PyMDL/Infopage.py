ce8d07e5561b01fe2119a4fef050ebfb90386cd1
Shivanarayan <shivnarayan19999@gmail.com>
2021-10-16 22:07:02 +0530

    Bug fixes

--- PyMDL/Infopage.py
+++ PyMDL/Infopage.py
@@ -4,56 +4,69 @@ import requests
 
 class InfoPage:
     def __init__(self, details):
-        self.info = details
-        self.title = details['title']
-        self.thumbnail = details['thumbnail']
-        self.type = details['type']
-        self.synopsis = details['synopsis']
-        self.casts = details['casts']
-        self.details = {**details['details'], **details['sdetails']}
+        self.info = details.copy()
+        self.info.pop('reco')
+        self.info.pop('reviews')
+        self.info.pop('synopsis')
+
+        self.details = details
+
+        allkeys = self.details.keys()
+
+        if 'title' in allkeys:
+            self.title = self.details['title']
+        if 'thumbnail' in allkeys:
+            self.thumbnail = self.details['thumbnail']
+        if 'type' in allkeys:
+            self.type = self.details['type']
+        if 'synopsis' in allkeys:
+            self.synopsis = self.details['synopsis']
+        if 'casts' in allkeys:
+            self.casts = self.details['casts']
+        if 'native title' in allkeys:
+            self.native = self.details['native title'].strip()
+        if 'genres' in allkeys:
+            self.genre = self.details['genres'].strip()
+        if 'duration' in allkeys:
+            self.duration = self.details['duration'].strip()
+        if 'country' in allkeys:
+            self.country = self.details['country'].strip()
+        if 'also known as' in allkeys:
+            self.aka = self.details['also known as'].split(",")
+        if 'director' in self.details.keys():
+            self.director = self.details['director']
+            del self.details['director']
+        if 'screenwriter' in self.details.keys():
+            self.screenwriter = self.details['screenwriter']
+            del self.details['screenwriter']
+        if 'screenwriter & director' in self.details.keys():
+            self.director = self.screenwriter = self.details['screenwriter & director']
+            del self.details['screenwriter & director']
+        del self.details['native title'], self.details['genres'], self.details['duration'], \
+            self.details['country'], self.details['also known as']
+
         self.releasedate = self.__release()
-        self.native = self.details['Native Title'].strip()
-        self.genre = self.details['Genres'].strip()
-        self.duration = self.details['Duration'].strip()
-        self.country = self.details['Country'].strip()
-        self.aka = self.details['Also Known As'].split(",")
-        if 'Director' in self.details.keys():
-            self.director = self.details['Director']
-            del self.details['Director']
-        if 'Screenwriter' in self.details.keys():
-            self.screenwriter = self.details['Screenwriter']
-            del self.details['Screenwriter']
-        if 'Screenwriter & Director' in self.details.keys():
-            self.director = self.screenwriter = self.details['Screenwriter & Director']
-            del self.details['Screenwriter & Director']
-        del self.details['Native Title'], self.details['Genres'], self.details['Duration'], \
-            self.details['Country'], self.details['Also Known As']
-        self.rec = details['reco']
-        self.reviews = details['reviews']
+
+        if details['reco']:
+            self.rec = self.details['reco']
+        if details['reviews']:
+            self.reviews = self.details['reviews']
 
     def __str__(self):
         return str(self.info)
 
     def __release(self):
+        date = "Not Recorded Yet"
         if self.type == "Movie":
-            date = self.details['Release Date'].strip()
-            del self.details['Release Date']
+            if 'release date' in self.details.keys():
+                date = self.details['release date'].strip()
+                del self.details['release date']
             return date
         else:
-            date = self.details['Aired'].strip()
-            del self.details['Aired']
+            if 'aired' in self.details.keys():
+                date = self.details['aired'].strip()
+                del self.details['aired']
             return date
-
-    def __break(self):
-        broken = [self.details['Native Title'].strip(),
-                  self.details['Genres'].strip(),
-                  self.details['Duration'].strip(),
-                  self.details['Country'].strip(),
-                  self.details['Also Known As'].split(","),
-                  self.details['Screenwriter & Director'].strip()]
-        del self.details['Native Title'], self.details['Genres'], self.details['Duration'], \
-            self.details['Country'], self.details['Also Known As'], self.details['Screenwriter & Director']
-        return broken
 
 
 def info(link: str):
@@ -75,23 +88,25 @@ def info(link: str):
         details['ratings'] = mainbox.find("div", class_="row no-gutter").find("div", class_="hfs").text
         details['synopsis'] = mainbox.find("p").text.replace('\n', ' ')
         minibox = mainbox.find("div", class_="show-detailsxss").find("ul").find_all("li")
-        details['details'] = {}
         for item in minibox[:5]:
-            details['details'][item.text.split(":")[0]] = item.text.split(":")[1]
+            try:
+                if item.text.split(":")[0].lower() == 'tags':
+                    continue
+                details[item.text.split(":")[0].lower()] = item.text.split(":")[1]
+            except IndexError:
+                continue
         castbox = soup.find("div", class_="box clear").find("div", class_="p-a-sm").find_all("b")
         casts = []
         for item in castbox:
             casts.append(item.text)
         details['casts'] = casts
         sidebox = soup.find("div", class_="box-body light-b").find_all("li")
-        details['sdetails'] = {}
-        getkey = []
-        for item in sidebox:
-            details['sdetails'][item.text.split(":")[0]] = item.text.split(":")[1]
-            getkey.append(item.text.split(':')[0])
-        del details['sdetails'][getkey[0]]
+        for item in sidebox[1:]:
+            details[item.text.split(":")[0].lower()] = item.text.split(":")[1]
+        if 'Duration' not in details.keys():
+            details['duration'] = "Not Recorded Yet"
         # Checking if it is a Movie or Drama
-        if 'Episodes' in details['sdetails'].keys():
+        if 'Episodes' in details.keys():
             details['type'] = "Drama"
         else:
             details['type'] = "Movie"
@@ -113,9 +128,10 @@ def info(link: str):
             tofilter = item.find("div", class_="review-body")
             tags = str(tofilter).find("</p>") + 4
             details['reviews'].append(str(tofilter)[tags:str(tofilter).find("<p", tags)].replace("<br/>", "").strip())
+        # Work on Review Filter
         return InfoPage(details)
 
 
 if __name__ == "__main__":
-    var = info("/35187-water-hole")
-    print(var.rec)
+    var = info("/40257-round-six")
+    print(var)
