15a9c20ca7156a08c1dd16d1fd5e520a383bbedb
Shivanarayan <shivnarayan19999@gmail.com>
2021-10-29 18:57:28 +0530

    Changes

--- PyMDL/Infopage.py
+++ PyMDL/Infopage.py
@@ -11,7 +11,7 @@ class InfoPage:
         self.details = details
         allkeys = self.details.keys()
         self.url = self.details.pop('url')
-        
+
         if 'title' in allkeys:
             self.title = self.details.pop('title')
         else:
